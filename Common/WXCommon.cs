cfbfeade21bac584c2b3ddccde2d0f9f9babfacf
mahuan <mahuan@beisen.com>
2021-04-08 16:25:38 +0800

    d

--- Common/WXCommon.cs
+++ Common/WXCommon.cs
@@ -9,7 +9,7 @@ namespace Common
     {
         public static string WX_TOKEN = System.Configuration.ConfigurationManager.AppSettings["WXToken"];
 
-        public static string WXEncodingAesKey = System.Configuration.ConfigurationManager.AppSettings["WXEncodingAesKey"];
+        //public static string WXEncodingAesKey = System.Configuration.ConfigurationManager.AppSettings["WXEncodingAesKey"];
         public static string WXAppId = System.Configuration.ConfigurationManager.AppSettings["WXAppId"];
         public static string WXAppSecret = System.Configuration.ConfigurationManager.AppSettings["WXAppSecret"];
         /// <summary>
